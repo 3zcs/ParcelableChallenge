307fdbed25139dade94702b8fab18ee6ae388718
Abdulaziz alawshan <abdulazizalawshan@Abdulazizs-MacBook-Pro.local>
2017-11-06 20:04:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/udacity/mena/parcelablechallenge/ShowStudentActivity.java
@@ -0,0 +1,14 @@
+package com.example.udacity.mena.parcelablechallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowStudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_student);
+        //TODO receive the list and print first item as Toast
+    }
+}
